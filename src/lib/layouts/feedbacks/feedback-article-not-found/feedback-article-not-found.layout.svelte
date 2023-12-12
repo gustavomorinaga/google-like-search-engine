0230ba120678bee6552fdb0434d3b4ef3bf9c231
Gustavo Morinaga <me@gustavomorinaga.dev>
2023-12-12 19:49:01 -0300

    refactor: swap emoji to icon in feedback-article-not-found

--- src/lib/layouts/feedbacks/feedback-article-not-found/feedback-article-not-found.layout.svelte
+++ src/lib/layouts/feedbacks/feedback-article-not-found/feedback-article-not-found.layout.svelte
@@ -1,10 +1,13 @@
 <script lang="ts">
 	import * as Alert from '$lib/components/ui/alert';
+	import { AlertCircle } from 'lucide-svelte';
 </script>
 
 <Alert.Root>
-	<span class="absolute">⚠</span>
-	<div class="ml-8">
+	<span class="absolute top-4 left-4">
+		<AlertCircle class="w-6 h-6" />
+	</span>
+	<div class="ml-2">
 		<Alert.Title>No articles found.</Alert.Title>
 		<Alert.Description>Try to search for another article.</Alert.Description>
 	</div>
