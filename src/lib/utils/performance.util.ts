ff0ca81c6b3781924982e898499825f71faa7b29
Gustavo Morinaga <me@gustavomorinaga.dev>
2023-12-12 22:16:26 -0300

    refactor: transform performance util to class

--- src/lib/utils/performance.util.ts
+++ src/lib/utils/performance.util.ts
@@ -1,42 +1,65 @@
 /**
  * Represents the names of performance marks.
  */
-export type TPerformanceMarkNames = [`start-${string}`, `end-${string}`, `duration-${string}`];
-
-/**
- * Clears the performance marks and measures for a given set of mark names.
- * @param marks - The names of the performance marks.
- */
-export const clearPerformance = ([start, end, duration]: TPerformanceMarkNames) => {
-	performance.clearMarks(start);
-	performance.clearMarks(end);
-	performance.clearMeasures(duration);
+export type TPerformanceMarkNames = {
+	start: `start-${string}`;
+	end: `end-${string}`;
+	duration: `duration-${string}`;
 };
 
 /**
- * Starts a performance measurement by creating a performance mark with the specified name.
- * @param markName - The name of the performance mark.
+ * Performance class for managing performance marks and measures.
  */
-export const startPerformance = (markName: string) => {
-	return performance.mark(markName);
-};
+export class PerformanceUtil {
+	/**
+	 * The names of the performance marks.
+	 */
+	private markNames: TPerformanceMarkNames;
 
-/**
- * Ends the performance measurement by marking the specified performance mark.
- * @param markName - The name of the performance mark to end.
- */
-export const endPerformance = (markName: string) => {
-	return performance.mark(markName);
-};
+	/**
+	 * The elapsed time of the performance measurement.
+	 */
+	private elapsedTime: number = 0;
 
-/**
- * Measures the performance between two marks and returns the performance entry.
- * @param markNames - The names of the marks used to measure the performance.
- * @returns The performance entry for the measured duration.
- */
-export const measurePerformance = ([start, end, duration]: TPerformanceMarkNames) => {
-	performance.measure(duration, start, end);
-	const [entry] = performance.getEntriesByName(duration);
+	constructor(markNames: TPerformanceMarkNames) {
+		this.markNames = markNames;
+	}
 
-	return entry;
-};
+	public getElapsedTime(): number {
+		return this.elapsedTime;
+	}
+
+	/**
+	 * Clears the performance marks and measures for a given set of mark names.
+	 */
+	public clearPerformance(): void {
+		performance.clearMarks(this.markNames.start);
+		performance.clearMarks(this.markNames.end);
+		performance.clearMeasures(this.markNames.duration);
+	}
+
+	/**
+	 * Starts a performance measurement by creating a performance mark with the specified name.
+	 */
+	public startPerformance(): PerformanceMark {
+		return performance.mark(this.markNames.start);
+	}
+
+	/**
+	 * Ends the performance measurement by marking the specified performance mark.
+	 */
+	public endPerformance(): PerformanceMark {
+		return performance.mark(this.markNames.end);
+	}
+
+	/**
+	 * Measures the performance between two marks and returns the performance entry.
+	 * @returns The performance entry for the measured duration.
+	 */
+	public measurePerformance(): PerformanceEntry {
+		performance.measure(this.markNames.duration, this.markNames.start, this.markNames.end);
+		const [elapsedTime] = performance.getEntriesByName(this.markNames.duration);
+		this.elapsedTime = elapsedTime.duration;
+		return elapsedTime;
+	}
+}
--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,41 +1,35 @@
-import {
-	startPerformance,
-	endPerformance,
-	clearPerformance,
-	measurePerformance,
-	sortByDate,
-	SearchEngine,
-	type TPerformanceMarkNames
-} from '$lib/utils';
+import { sortByDate, PerformanceUtil, SearchEngine } from '$lib/utils';
 import type { TArticle } from '$lib/ts';
 
-export const load = async ({ fetch, url }) => {
+export const load = async ({ fetch, url, setHeaders }) => {
 	const data = await fetch(`${url.origin}/db/articles.data.json`)
 		.then<Array<TArticle>>((res) => res.json())
 		.then((res) => res.sort(sortByDate));
 
 	const searchTerm = url.searchParams.get('search') ?? '';
 
-	const performanceMarkNames: TPerformanceMarkNames = [
-		`start-${searchTerm}`,
-		`end-${searchTerm}`,
-		`duration-${searchTerm}`
-	];
-	const [markNameStart, markNameEnd] = performanceMarkNames;
+	setHeaders({
+		'Cache-Control': 'max-age=60, s-max-age=60, stale-while-revalidate=60, public'
+	});
 
-	let elapsedTime = 0;
-	startPerformance(markNameStart);
+	const searchPerformance = new PerformanceUtil({
+		start: `start-${searchTerm}`,
+		end: `end-${searchTerm}`,
+		duration: `duration-${searchTerm}`
+	});
 
 	const articleSearchEngine = new SearchEngine(data, {
 		fields: ['title', 'description', 'content'],
 		options: { highlight: Boolean(searchTerm) }
 	});
 
+	searchPerformance.startPerformance();
+
 	const articles = await articleSearchEngine.search(searchTerm).finally(() => {
-		endPerformance(markNameEnd);
-		elapsedTime = measurePerformance(performanceMarkNames).duration;
-		clearPerformance(performanceMarkNames);
+		searchPerformance.endPerformance();
+		searchPerformance.measurePerformance();
+		searchPerformance.clearPerformance();
 	});
 
-	return { articles, elapsedTime };
+	return { articles, elapsedTime: searchPerformance.getElapsedTime() };
 };
