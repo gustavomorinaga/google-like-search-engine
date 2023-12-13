d5f8c6d5ddbd97cf6f7c2273487dbb75e318b419
Gustavo Morinaga <me@gustavomorinaga.dev>
2023-12-13 00:51:17 -0300

    perf: adding robots.txt and fix heading accessibility

--- src/lib/layouts/cards/card-article/card-article.layout.svelte
+++ src/lib/layouts/cards/card-article/card-article.layout.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import * as Card from '$lib/components/ui/card';
+	import { ArrowRight } from 'lucide-svelte';
 	import type { TArticle } from '$lib/ts';
 
 	export let article: TArticle;
@@ -10,16 +11,16 @@
 <Card.Root class="hover:border-primary transition-colors group">
 	<Card.Header class="flex-row justify-between space-y-0">
 		<div class="flex flex-col gap-2">
-			<Card.Title>{@html title}</Card.Title>
+			<Card.Title tag="h2">{@html title}</Card.Title>
 			<Card.Description>{@html description}</Card.Description>
 		</div>
 
 		<div class="flex flex-col gap-2">
 			<span>{formattedCreatedAt}</span>
 			<span
-				class="text-lg ml-auto text-primary -translate-x-4 group-hover:translate-x-0 transition opacity-0 group-hover:opacity-100 -mt-2"
+				class="-translate-x-4 group-hover:translate-x-0 transition opacity-0 group-hover:opacity-100 ml-auto my-auto"
 			>
-				➞
+				<ArrowRight class="w-6 h-6 text-primary" />
 			</span>
 		</div>
 	</Card.Header>
